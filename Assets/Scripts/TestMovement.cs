0fde1a059837dedcfa8bfa1d13b601e2d786034a
Sebastian Thuemmel <sebastianthuemmel@yahoo.ca>
2021-09-04 17:00:30 +0200

    add footstep sounds

--- Assets/Scripts/TestMovement.cs
+++ Assets/Scripts/TestMovement.cs
@@ -3,13 +3,23 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.Collections;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class TestMovement : MonoBehaviour
 {
+    [SerializeField] AudioClip[] footsteps;
     [SerializeField] private float speed;
+    [SerializeField] float footstepDelay = 0.4f;
+    
     private Vector3 movingDirection;
+    float timeStamp;
+    AudioSource source;
+    
     private void Start()
     {
+        timeStamp = Time.time;
+        source = GetComponent<AudioSource>();
+        source.clip = footsteps[0];
         movingDirection = Vector3.zero;
     }
 
@@ -40,6 +50,17 @@ public class TestMovement : MonoBehaviour
 
     private void Update()
     {
+        if (movingDirection != Vector3.zero && (Time.time > timeStamp + footstepDelay))
+        {
+            timeStamp = Time.time;
+            if (Random.Range(0,3) != 0)
+            {
+                int index = Random.Range(0, 5);
+                source.clip = footsteps[index];
+            }
+            source.Play();
+            Debug.Log("footstep");
+        }
         transform.position += movingDirection * speed * Time.deltaTime;
     }
 }
